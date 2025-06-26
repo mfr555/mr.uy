78006278f2eb8734b7dc3093b67330099955fb1d
Matías Rodriguez <mfrodriguez555@gmail.com>
2025-06-26 12:45:13 -0300

    fix whatsapp js

--- public/js/whatsappform.js
+++ public/js/whatsappform.js
@@ -1,13 +1,13 @@
 document.querySelector('#mensaje').addEventListener('input', function() {
     const text = this.value;
     const name = document.querySelector('#nombre').value;
-    const url = `https://wa.me/59897807203?text=${encodeURIComponent(text)}%0A%0A${encodeURIComponent(name)}`;
+    const url = `https://wa.me/59897807023?text=${encodeURIComponent(text)}%0A%0A${encodeURIComponent(name)}`;
     document.querySelector('#enviarwpp').href = url;
 });
 
 document.querySelector('#nombre').addEventListener('input', function() {
     const text = document.querySelector('#mensaje').value;
     const name = this.value;
-    const url = `https://wa.me/59897807203?text=${encodeURIComponent(text)}%0A%0A${encodeURIComponent(name)}`;
+    const url = `https://wa.me/59897807023?text=${encodeURIComponent(text)}%0A%0A${encodeURIComponent(name)}`;
     document.querySelector('#enviarwpp').href = url;
 });
